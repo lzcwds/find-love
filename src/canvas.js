2d3da7e362e9cdc017226f2409f6f72864a5337a
duyu <duyu@kedacom.com>
2018-07-31 19:16:36 +0800

    init

---
+++ src/canvas.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dy on 2018/7/31.
+ *
+ */
+
+class ICanvas{
+    constructor(option){
+        let defaultOption = {
+
+        }
+        this.option = Object.assign(option, defaultoption);
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "canvas.js"
+        filename: "js/canvas.js"
     },
 
     module: {
